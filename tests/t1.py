1b24cfa34ebbeaf72ed13af89f50e3f7bfac21e7
Roland Kwitt <rkwitt@gmx.at>
2025-08-07 10:49:06 +0200

    ADD: ExSheet1 for CV WS2526

---
+++ tests/t1.py
@@ -0,0 +1,13 @@
+from otter.test_files import test_case
+import torch
+
+OK_FORMAT = False
+
+name = "Exercise 1.1"
+points = 2
+
+
+@test_case(points=2)
+def test_1(assignment_ex1, env):
+    T = torch.load('assets/ex1_T.pt', weights_only=False)
+    assert (T-env['assignment_ex1']().float()).norm() < 1e-6
